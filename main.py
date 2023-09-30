f3acea2297ca2146abc0b0b551e057e6d3b1d8aa
AdityaY9 <amyedurkar_b22@it.vjti.ac.in>
2023-09-30 10:08:27 +0530

    Training Done

--- main.py
+++ main.py
@@ -35,7 +35,110 @@ def faceDetection(test_img):
 
     # to prevent false positives
 
-    faces = face_haar_cascade.detectMultiScale(gray_img, scaleFactor=1.32, minNeighbors=5)
+    faces = face_haar_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=6)
     return faces, gray_img
+
+def labels_for_training_data(directory):
+    faces = []
+    faceID = []
+
+    # loop through each directory and read images within it
+    for path,subdirnames,filenames in os.walk(directory):
+        # accessing folders
+        for filename in filenames:
+            # accessing individual images
+            if filename.startswith("."):
+                print("Skipping system file")
+                continue
+            # loading images
+            # using opencv imread function
+            # imread returns a numpy array
+            # numpy array is stored in img
+            id = os.path.basename(path)
+            img_path = os.path.join(path, filename)
+            print("img_path:", img_path)
+            print("id:", id)
+            test_img = cv2.imread(img_path)
+            if test_img is None:
+                print("Image not loaded properly")
+                continue
+            # calling faceDetection function
+            # returns faces detected in particular image
+            # gray image is returned
+            faces_rect, gray_img = faceDetection(test_img)
+            # if there is no face detected
+            # it will return the same image
+            # so we will ignore it
+
+            # we need single face in image
+            # to train our model
+            # classifier will not be able to detect multiple faces
+            # it will get confused
+            # so we will ignore images with multiple faces
+            if len(faces_rect) != 1:
+                continue
+
+            (x,y,w,h) = faces_rect[0]   
+
+
+            # classifier takes only the region of interest
+            # cropping the face part to pass it to classifier
+            # classifier takes only gray image
+
+            roi_gray = gray_img[y:y+w, x:x+h]
+
+
+            # convert ot numpy array
+            # classifier takes numpy array
+            # converting list to numpy array
+            # append the face to faces list
+            faces.append(roi_gray)
+            faceID.append(int(id))
+    return faces, faceID
+
+def train_classifier(faces,faceID):
+    # create object of face recognizer
+    # using opencv
+    # LBPH is face recognizer
+    # it is fast and accurate
+    # it labels the pixels of image
+    # it uses local features to predict face
+    # local binary patterns histogram
+    # binary
+    # pixels are converted to binary
+    # based on central pixel
+
+
+
+    # if neighbor pixel is greater than central pixel
+    # create object of face recognizer
+    face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+    # train the face recognizer
+    # using numpy array of faces and faceID
+    face_recognizer.train(faces, np.array(faceID))
+    return face_recognizer
+
+def draw_rect(test_img, face):
+    (x,y,w,h) = face
+    # draw a rectangle around the face
+    # using opencv rectangle function
+    # rectangle takes 5 arguments
+    # 1. image
+    # 2. top left coordinates
+    # 3. bottom right coordinates
+    # 4. color
+    # 5. thickness
+    cv2.rectangle(test_img, (x,y), (x+w, y+h), (255,102,0), thickness=2, lineType=2, shift=0)
+
+def put_text(test_img, text, x, y):
+    # put text on the image
+    # using opencv putText function
+    # putText takes 5 arguments
+    # 1. image
+    # 2. text
+    # 3. coordinates
+    # 4. font
+    # 5. font size
+    cv2.putText(test_img, text, (x,y), cv2.FONT_HERSHEY_DUPLEX, 1, (255,102,0), 2)
 
 
--- tester.py
+++ tester.py
@@ -5,20 +5,73 @@ import main as m
 
 # this function will read all persons' training images, detect face from each image
 # used to load an image from a file
-test_img = cv2.imread(r"C:\Users\MSHOME\Desktop\Newfolder\FaceRecognition\Images\Aditya.test.jpg")
+test_img = cv2.imread(r"C:\Users\MSHOME\Desktop\Newfolder\FaceRecognition\Images\Aditya.jpg")
+# vid = cv2.VideoCapture(r"C:\Users\MSHOME\Desktop\Newfolder\FaceRecognition\video\Video.mp4")
+# running = True
+# while running:
+#     success, frame = vid.read()
+#     resized_img = cv2.resize(frame, (1000,700))
+#     faces_detected, gray_img = m.faceDetection(resized_img)
+#     for(x,y,w,h) in faces_detected:
+#         cv2.rectangle(resized_img, (x,y), (x+w, y+h), (255,102,0), thickness=2, lineType=8, shift=0)
+#     cv2.imshow("face detection tutorial", resized_img)
+#     cv2.waitKey(1)
+    
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+# vid.release()
+# cv2.destroyAllWindows()
 
 # detect faces from test image
 
 # collect the rectangles returned by faceDetection function
-# collect the gray image returned by faceDetection function
+# # collect the gray image returned by faceDetection function
 faces_detected, gray_img = m.faceDetection(test_img)
 
-print("faces_detected:", faces_detected)
+# print("faces_detected:", faces_detected)
 
-for(x,y,w,h) in faces_detected:
-    cv2.rectangle(test_img, (x,y), (x+w, y+h), (255,102,0), thickness=2, lineType=2, shift=0)
+# for(x,y,w,h) in faces_detected:
+#     cv2.rectangle(test_img, (x,y), (x+w, y+h), (255,102,0), thickness=2, lineType=2, shift=0
 
 # resized_img = cv2.resize(test_img, (1000,700))
-cv2.imshow("face detection tutorial", test_img)
+# cv2.imshow("face detection tutorial", resized_img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+faces, faceID = m.labels_for_training_data(r"C:\Users\MSHOME\Desktop\Newfolder\FaceRecognition\TrainingImages")
+# face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+# face_recognizer.read(r"C:\Users\MSHOME\Desktop\Newfolder\FaceRecognition\trainingData.yml")
+face_recognizer = m.train_classifier(faces, faceID)
+# m.save("trainingData.yml")
+
+name = {0:"Ranbir", 1:"Aditya"}
+
+for faces in faces_detected:
+    (x,y,w,h) = faces
+    # extracting region of interest
+    roi_gray = gray_img[y:y+h, x:x+h]
+    # predicting the label of given image
+    # confidence is the accuracy of the prediction
+    # confidence is a number between 0 and 100
+    # the lower the value, the more accurate the prediction
+    # label 0 or 1
+    # confidence value lower than its more accurate
+    # 35 is the threshold value for confidence
+    label, confidence = face_recognizer.predict(roi_gray)
+    print("confidence:", confidence)
+    print("label:", label)
+    m.draw_rect(test_img, faces)
+
+    # extract the name from the dictionary
+    predicted_name = name[label]
+
+
+    m.put_text(test_img, predicted_name, x, y)
+
+resized_img = cv2.resize(test_img, (1000,700))
+cv2.imshow("face detection tutorial", resized_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+
